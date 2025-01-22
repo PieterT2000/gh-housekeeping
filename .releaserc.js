39e03f9d9ea5a1a857dcdd0fe8adb433a7d77e74
PieterT2000 <piet@mapmortar.io>
2025-01-22 17:50:38 +0000

    chore: configure semantic-release plugins explicitly

--- .releaserc.js
+++ .releaserc.js
@@ -1,4 +1,13 @@
 export default {
   branches: ['main'],
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    ['@semantic-release/git', {
+      assets: ['dist/'],
+      message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}'
+    }],
+    '@semantic-release/github'
+  ],
   tagFormat: '${version}'
 }; 
